e506af44b0f9568330bf71a52a369b2688ba0902
Mithat Akbulut <marmarisco860@gmail.com>
2022-01-23 15:51:05 +0300

    Get all sets and words from firestore database to show onto discover screen.

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import BottomNavigationBar from '../components/BottomNavigationBar';
 import TopBar from '../components/TopBar';
+import { collection, getDoc, getDocs } from "firebase/firestore";
+import { db } from '../firebase';
 
 export default function DiscoverScreen({navigation}) {
       const theme = useSelector(state=>state.theme.darkTheme);
+      const [allSets, setAllSets] = useState([]);
+      const [allWords, setAllWords] = useState([]);
+      const [loading, setLoading] = useState(true);
+
+      function getAllSets(){
+        getDocs(collection(db, "sets")).then(res=>{
+          res.forEach(doc=>{
+            setAllSets(prev=>[...prev,{id:doc.id, title:doc.data().title, userId:doc.data().userId }])
+          })
+        }).then(()=>{
+          getAllWords();
+        });
+      }
+
+      function getAllWords(){
+        getDocs(collection(db, 'words')).then(res=>{
+          res.forEach(doc=>{
+            setAllWords(prev=>[...prev, {id:doc.id, data:doc.data()}])
+          })
+        }).then(()=>setLoading(false));
+      }
+
+      
+
+      useEffect(() => {
+        if(allSets.length===0){
+          getAllSets();
+        }
+      }, []);
+
+      
+      if(loading){
+        return <View style={{justifyContent:"center", alignItems:"center",flex:1}}>
+          <Text>LOADING</Text>
+          
+        </View>
+      }
 
   return <View style={{
     height: Dimensions.get("screen").height,
@@ -15,7 +54,16 @@ export default function DiscoverScreen({navigation}) {
     alignItems:"center",
     flex:1
   }}>
-      <Text>Discover</Text>
+      <View> 
+      <Text>DISCOVER {allSets.length} {allWords.length}</Text> 
+
+      {allSets.map(set=>{
+        return <View key={set.id}>
+          <Text>{set.title}</Text>
+        </View>
+      })}
+      
+      </View>
       <BottomNavigationBar navigation={navigation}/>
       <TopBar/>
   </View>
